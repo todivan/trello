227c6d8a46f0dc3f5052df0955993a9fc25309a8
Ivan Todorovic <ivan.todorovic@cleverbridge.com>
2024-03-12 11:35:26 +0100

    Trello: task menu

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
-import { OutlinedInput, TextField } from "@mui/material";
+import { ListItemIcon, ListItemText, Menu, MenuItem, OutlinedInput, TextField } from "@mui/material";
 import { TTask } from "../../types/CommonTypes";
 import ModeIcon from '@mui/icons-material/Mode';
+import React from "react";
+import NorthIcon from '@mui/icons-material/North';
+import SouthIcon from '@mui/icons-material/South';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 type TTaskProps = {
     key: React.Key;
@@ -30,8 +34,19 @@ const Task: React.FC<TTaskProps> = ({ task, updateTask, isFocusOnNew }) => {
      }
 
     const openTaskDetails = () => {
+        handleMenuClose();
         alert("Task details \n\n Name: " + task.name + "\n\n Description: " + task.description);
     }
+
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+      setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+      setAnchorEl(null);
+    };
 
     return(
         <>
@@ -54,20 +69,50 @@ const Task: React.FC<TTaskProps> = ({ task, updateTask, isFocusOnNew }) => {
                 }}
             />
             :
-            <OutlinedInput
-                id="outlined-adornment-weight"
-                multiline
-                minRows={1}
-                maxRows={8}
-                defaultValue={task.name}
-                endAdornment={<ModeIcon fontSize="small" onClick={switchToEdit} cursor='pointer'></ModeIcon>}
-                aria-describedby="outlined-weight-helper-text"
-                sx={{ border: '0px solid white', borderRadius:3, width:250, cursor: 'pointer', color:'white'}}
-                onDoubleClick={openTaskDetails}
-                inputProps={{
-                'aria-label': 'weight', readOnly: true,
-                }}
-            />
+            <>
+                <OutlinedInput
+                    id="outlined-adornment-weight"
+                    multiline
+                    minRows={1}
+                    maxRows={8}
+                    defaultValue={task.name}
+                    endAdornment={<ModeIcon fontSize="small" onClick={switchToEdit} cursor='pointer'></ModeIcon>}
+                    aria-describedby="outlined-weight-helper-text"
+                    sx={{ border: '0px solid white', borderRadius:3, width:250, cursor: 'pointer', color:'white', '&:hover': {cursor: 'pointer',}}}
+                    onClick={handleClick}
+                    inputProps={{
+                    'aria-label': 'weight', readOnly: true,
+                    }}
+                />
+                <Menu
+                    id="basic-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleMenuClose}
+                    MenuListProps={{
+                    'aria-labelledby': 'basic-button',
+                    }}
+                >
+                    <MenuItem onClick={handleMenuClose}>
+                        <ListItemIcon>
+                            <NorthIcon fontSize="small" />
+                        </ListItemIcon>
+                        <ListItemText>Move Up</ListItemText>
+                    </MenuItem>
+                    <MenuItem onClick={handleMenuClose}>
+                        <ListItemIcon>
+                            <SouthIcon fontSize="small" />
+                        </ListItemIcon>
+                        <ListItemText>Move Down</ListItemText>
+                    </MenuItem>
+                    <MenuItem onClick={openTaskDetails}>                        
+                        <ListItemIcon>
+                            <MoreHorizIcon fontSize="small" />
+                        </ListItemIcon>
+                        <ListItemText>Details</ListItemText>
+                    </MenuItem>
+                </Menu>
+            </>
             }
         </>
     );
